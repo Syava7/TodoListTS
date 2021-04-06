0be0b76fb2999ece4354abdf3a6044e96f3dc1e4
Syava7 <syava77787@gmail.com>
2021-04-06 12:20:39 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import TodoList from "./TodoList";
+
+export type TaskType = {
+    id: number,
+    isDone: boolean,
+    title: string
+}
+
+function App() {
+    const taskToLearn: Array<TaskType> = [
+        { id: 1, isDone: true, title: 'React'},
+        { id: 2, isDone: true, title: 'CSS'},
+        { id: 3, isDone: false, title: 'VUE'}
+    ]
+
+    const taskToBuy: Array<TaskType> = [
+        { id: 4, isDone: true, title: 'ReactHooks'},
+        { id: 5, isDone: true, title: 'CSS3'},
+        { id: 6, isDone: false, title: 'VUE3'}
+    ]
+
+    return (
+        <div className="App">
+           <TodoList title={'Hello word'}
+                     tasks={ taskToLearn }/>
+           <TodoList title={'Hello word'}
+                     tasks={ taskToBuy }/>
+        </div>
+    );
+}
+
+export default App;
